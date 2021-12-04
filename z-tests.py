e44e3f3185ddcc4d9f24ffbbc519f65825acfecb
sharvisharma <69799445+sharvisharma@users.noreply.github.com>
2021-12-04 13:04:58 +0530

    Add files via upload

---
+++ z-tests.py
@@ -0,0 +1,63 @@
+import plotly.figure_factory as ff
+import plotly.graph_objects as go
+import statistics
+import random
+import pandas as pd
+import csv
+
+df = pd.read_csv("medium_data.csv")
+data = df["reading_time"].tolist()
+
+
+# fig = ff.create_distplot([data],["Reading Time"], show_hist= False)
+# fig.show()
+
+
+mean = statistics.mean(data)
+std_deviation = statistics.stdev(data)
+
+
+def random_set_of_mean(counter):
+    dataset = []
+    for i in range(0, counter):
+        random_index= random.randint(0,len(data)-1)
+        value = data[random_index]
+        dataset.append(value)
+    mean = statistics.mean(dataset)
+    return mean
+
+
+mean_list = []
+for i in range(0,1000):
+    set_of_means= random_set_of_mean(100)
+    mean_list.append(set_of_means)
+
+
+std_deviation = statistics.stdev(mean_list)
+mean = statistics.mean(mean_list)
+print("mean of sampling distribution:- ",mean)
+print("Standard deviation of sampling distribution:- ", std_deviation)
+
+
+fig = ff.create_distplot([mean_list], ["reading time"], show_hist=False)
+fig.add_trace(go.Scatter(x=[mean, mean], y=[0, 0.20], mode="lines", name="MEAN"))
+fig.show()
+# print("Standard deviation of sampling distribution:- ", std_deviation)
+
+
+
+first_std_deviation_start, first_std_deviation_end = mean-std_deviation, mean+std_deviation
+second_std_deviation_start, second_std_deviation_end = mean-(2*std_deviation), mean+(2*std_deviation)
+third_std_deviation_start, third_std_deviation_end = mean-(3*std_deviation), mean+(3*std_deviation)
+print("std1",first_std_deviation_start, first_std_deviation_end)
+print("std2",second_std_deviation_start, second_std_deviation_end)
+print("std3",third_std_deviation_start,third_std_deviation_end)
+
+fig.add_trace(go.Scatter(x=[first_std_deviation_start, first_std_deviation_start], y=[0, 0.10], mode="lines", name="STANDARD DEVIATION 1 START"))
+fig.add_trace(go.Scatter(x=[first_std_deviation_end, first_std_deviation_end], y=[0, 0.10], mode="lines", name="STANDARD DEVIATION 1 END"))
+fig.add_trace(go.Scatter(x=[second_std_deviation_start, second_std_deviation_start], y=[0, 0.13], mode="lines", name="STANDARD DEVIATION 2 START"))
+fig.add_trace(go.Scatter(x=[second_std_deviation_end, second_std_deviation_end], y=[0, 0.13], mode="lines", name="STANDARD DEVIATION 2 END"))
+fig.add_trace(go.Scatter(x=[third_std_deviation_start,third_std_deviation_start], y=[0,0.15], mode="lines", name="STANDARD DEVIATION 3 START"))
+fig.add_trace(go.Scatter(x=[third_std_deviation_end,third_std_deviation_end], y=[0,0.15], mode="lines", name="STANDARD DEVIATION 3 END"))
+#fig.show()
+
